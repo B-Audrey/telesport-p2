7a3b76a12d09538cbe14ae332384a7d33a8416c1
Audrey <audrey.bonis@fuzeo.fr>
2024-09-09 16:36:52 +0200

    feat: add loader

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,19 +1,30 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { OlympicService } from '../../shared/services/olympic.service';
-import { tap } from 'rxjs/operators';
 import { NumberBlocComponent } from '../../shared/components/number-bloc/number-bloc.component';
 import { TitleComponent } from '../../shared/components/title/title.component';
+import { LoaderComponent } from '../../shared/components/loader/loader.component';
+import {catchError, of, tap} from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
-  imports: [AsyncPipe, CommonModule, NumberBlocComponent, TitleComponent],
+  imports: [AsyncPipe, CommonModule, NumberBlocComponent, TitleComponent, LoaderComponent],
 })
 export class DashboardComponent {
   private olympicService = inject(OlympicService);
+  errorMessage: string = '';
+  isLoading = signal(true);
 
-  olympicData$ = this.olympicService.getOlympics().pipe(tap(data => console.log(data)));
+  olympicData$ = this.olympicService.getOlympics().pipe(
+    catchError((error: HttpErrorResponse) => {
+      this.errorMessage = error.message;
+      this.isLoading.set(false);
+      return of(null);
+    }),
+      tap(() => this.isLoading.set(false))
+  );
 }
--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,40 +1,58 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import { NumberBlocComponent } from '../../shared/components/number-bloc/number-bloc.component';
 import { TitleComponent } from '../../shared/components/title/title.component';
 import { OlympicService } from '../../shared/services/olympic.service';
 import { AsyncPipe } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { OlympicCountry } from '../../shared/models/Olympic';
+import {catchError, of} from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import {LoaderComponent} from '../../shared/components/loader/loader.component';
+
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [NumberBlocComponent, TitleComponent, AsyncPipe],
+  imports: [NumberBlocComponent, TitleComponent, AsyncPipe, LoaderComponent],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss',
 })
 export class DetailsComponent implements OnInit {
   private olympicService = inject(OlympicService);
   private route = inject(ActivatedRoute);
+  isLoading = signal(true)
   olympic: OlympicCountry | undefined = undefined;
   totalMedals: number = 0;
   totalAthletes: number = 0;
+  errorMessage: string = '';
 
   ngOnInit(): void {
+    this.isLoading.set(true);
+    console.log(this.isLoading())
     if (this.route.snapshot.params['id']) {
-      this.olympicService.getOneOlympicCountry(this.route.snapshot.params['id']).subscribe(data => {
-        if (data) {
-          this.olympic = data;
-          this.totalMedals = data.participations.reduce(
-            (acc, current) => acc + (current.medalsCount || 0),
-            0,
-          );
-          this.totalAthletes = data.participations.reduce(
-            (acc, current) => acc + (current.athleteCount || 0),
-            0,
-          );
-        }
-      });
+      this.olympicService
+        .getOneOlympicCountry(this.route.snapshot.params['id'])
+        .pipe(
+          catchError((err: HttpErrorResponse) => {
+            this.isLoading.set(false);
+            this.errorMessage = err.message;
+            return of(null);
+          }),
+        )
+        .subscribe(data => {
+          if (data) {
+            this.olympic = data;
+            this.totalMedals = data.participations.reduce(
+              (acc, current) => acc + (current.medalsCount || 0),
+              0,
+            );
+            this.totalAthletes = data.participations.reduce(
+              (acc, current) => acc + (current.athleteCount || 0),
+              0,
+            );
+          }
+          this.isLoading.set(false);
+        });
     }
   }
 }
