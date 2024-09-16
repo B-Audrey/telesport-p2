62e75a553996d66be44414fa2bf093c1e11f724f
Audrey <audrey.bonis@fuzeo.fr>
2024-09-16 17:03:05 +0200

    feat: add go back button

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -3,7 +3,7 @@ import { NumberBlocComponent } from '../../shared/components/number-bloc/number-
 import { TitleComponent } from '../../shared/components/title/title.component';
 import { OlympicService } from '../../shared/services/olympic.service';
 import { AsyncPipe } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute, RouterLink} from '@angular/router';
 import { OlympicCountry } from '../../shared/models/Olympic';
 import { catchError, of } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -14,21 +14,27 @@ import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [NumberBlocComponent, TitleComponent, AsyncPipe, LoaderComponent, LineChartModule],
+  imports: [
+    NumberBlocComponent,
+    TitleComponent,
+    AsyncPipe,
+    LoaderComponent,
+    LineChartModule,
+    RouterLink,
+  ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss',
 })
 export class DetailsComponent implements OnInit {
   private olympicService = inject(OlympicService);
   private route = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef)
+  private destroyRef = inject(DestroyRef);
   isLoading = signal(true);
   olympic: OlympicCountry | undefined = undefined;
   totalMedals: number = 0;
   totalAthletes: number = 0;
   errorMessage: string = '';
   lineChartData: any[] = [];
-
 
   ngOnInit(): void {
     this.isLoading.set(true);
@@ -61,8 +67,8 @@ export class DetailsComponent implements OnInit {
                 series: data.participations.map(participation => ({
                   name: participation.year.toString(),
                   value: participation.medalsCount,
-                }))
-              }
+                })),
+              },
             ];
           }
           this.isLoading.set(false);
